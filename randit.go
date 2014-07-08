66dba56b1f2b004899b4ac5827e1d2b10e58dfbe
slouc <sinisalouc@gmail.com>
2014-07-08 19:05:08 -0400

    First commit

---
+++ randit.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "net/http"
+    "io/ioutil"
+//  "github.com/gorilla/mux"
+)
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+    //w.Header().Set("Content-Type","text/html; charset=utf-8")
+    //io.WriteString(w, readFile("/root/rand/index.html"))
+    http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func readFile(filename string) string {
+    content, _ := ioutil.ReadFile(filename) //handle err
+    return string(content)
+}
+
+func main() {
+    http.HandleFunc("/", serveHome);
+    http.ListenAndServe(":8080", nil)    
+}
